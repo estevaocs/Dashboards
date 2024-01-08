08b32410e74e774d82602e2cb6791654e4a9339e
Estevão Cristino da Silva <estevao_cs@hotmail.com>
2024-01-08 14:05:56 -0300

    Alterações para publicação no Heroku
    Alterações para publicação no Heroku

--- Python/Salesanalysis-dash/index.py
+++ Python/Salesanalysis-dash/index.py
@@ -519,4 +519,5 @@ def graph11(month, team, toggle):
 # Run server
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port=8050)
+    port = init(os.getenv('PORT'), '5000')
+    app.run(host='0.0.0.0', port=port)
